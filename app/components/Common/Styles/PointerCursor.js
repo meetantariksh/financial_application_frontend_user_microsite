8ee884d38eabf8e0a537f7d4e62f6f06fd884046
meetantariksh <meetantariksh@gmail.com>
2018-06-10 00:20:19 +0530

    Update

---
+++ app/components/Common/Styles/PointerCursor.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import React from 'react';
+
+class Link extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onLinkClick = this.onLinkClick.bind(this);
+  }
+
+  onLinkClick() {
+    if (this.props.onLinkClick && {}.toString.call(this.props.onLinkClick) === '[object Function]') {
+      this.props.onLinkClick();
+    }
+  }
+
+  render() {
+    return (
+      <a className={this.props.className} onClick={this.onLinkClick} role="tree" tabIndex={0}>
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+const StyledLinkCursor = styled(Link)`
+    cursor: pointer
+  `;
+
+Link.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.object,
+  onLinkClick: PropTypes.func,
+};
+
+export default StyledLinkCursor;
--- app/components/Common/TopMenu/index.js
+++ app/components/Common/TopMenu/index.js
@@ -6,28 +6,81 @@
 
 import React from 'react';
 // import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import { Link } from 'react-router-dom';
+
+import PointerStyledCursor from '../Styles/PointerCursor';
 
 import MaleAvatar from '../../../images/globalImage/male_avatar.png';
+
+const shortid = require('shortid');
 
 class TopMenu extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
     super(props);
 
+    this.state = {
+      isMailDropdownOpen: false,
+      isNotificationDropDownOpen: false,
+      isProfileDropdownOpen: false,
+    };
+
     this.toggleSideMenuBar = this.toggleSideMenuBar.bind(this);
+    this.toggleMailDropDown = this.toggleMailDropDown.bind(this);
+    this.toggleNotificationDropDown = this.toggleNotificationDropDown.bind(this);
+    this.toggleProfileDropdown = this.toggleProfileDropdown.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClick);
   }
 
   toggleSideMenuBar() {
 
   }
 
+  toggleMailDropDown() {
+    this.setState({
+      isMailDropdownOpen: !this.state.isMailDropdownOpen,
+    });
+  }
+
+  toggleNotificationDropDown() {
+    this.setState({
+      isNotificationDropDownOpen: !this.state.isNotificationDropDownOpen,
+    });
+  }
+
+  toggleProfileDropdown() {
+    this.setState({
+      isProfileDropdownOpen: !this.state.isProfileDropdownOpen,
+    });
+  }
+
+  handleClick() {
+    if (this.state.isMailDropdownOpen || this.state.isNotificationDropDownOpen || this.state.isProfileDropdownOpen) {
+      this.setState({
+        isMailDropdownOpen: false,
+        isNotificationDropDownOpen: false,
+        isProfileDropdownOpen: false,
+      });
+    }
+  }
+
   render() {
     return (
       <div>
         <header className="main-header">
-          <a href="../../index2.html" className="logo">
+          <Link to="/" className="logo">
             <span className="logo-mini"><b>P</b>C</span>
             <span className="logo-lg"><b>Periscope</b> Capitals</span>
-          </a>
+          </Link>
           <nav className="navbar navbar-static-top">
             <a className="sidebar-toggle" data-toggle="push-menu" role="button">
               <span className="sr-only">Toggle navigation</span>
@@ -38,120 +91,80 @@ class TopMenu extends React.Component { // eslint-disable-line react/prefer-stat
 
             <div className="navbar-custom-menu">
               <ul className="nav navbar-nav">
-                <li className="dropdown messages-menu">
-                  <a className="dropdown-toggle" data-toggle="dropdown">
+                <li className={`dropdown messages-menu ${this.state.isMailDropdownOpen ? 'open' : ''}`}>
+                  <PointerStyledCursor className="dropdown-toggle" onLinkClick={this.toggleMailDropDown} data-toggle="dropdown" aria-expanded={this.state.isMailDropdownOpen}>
                     <i className="fa fa-envelope-o"></i>
-                    <span className="label label-success">4</span>
-                  </a>
+                    <span className="label label-success">{this.props.messageData.totalMessages}</span>
+                  </PointerStyledCursor>
                   <ul className="dropdown-menu">
-                    <li className="header">You have 4 messages</li>
+                    <li className="header">You have {this.props.messageData.totalMessages} messages</li>
                     <li>
                       <ul className="menu">
-                        <li>
-                          <a>
-                            <div className="pull-left">
-                              <img src={MaleAvatar} className="img-circle" alt="User" />
-                            </div>
-                            <h4>
-                              Support Team
-                              <small><i className="fa fa-clock-o"></i> 5 mins</small>
-                            </h4>
-                            <p>Why not buy a new awesome theme?</p>
-                          </a>
-                        </li>
+                        {
+                          this.props.messageData.messageData.map((message) => (
+                            <li key={`__messages_top_menu_${shortid.generate()}`}>
+                              <a>
+                                <div className="pull-left">
+                                  <img src={message.image} className="img-circle" alt="User" />
+                                </div>
+                                <h4>
+                                  {message.from}
+                                </h4>
+                                <p>{message.subject}</p>
+                              </a>
+                            </li>
+                          ))
+                        }
                       </ul>
                     </li>
-                    <li className="footer"><a href="#">See All Messages</a></li>
+                    <li className="footer"><Link to="/" className="footer">See All Messages</Link></li>
                   </ul>
                 </li>
-                <li className="dropdown notifications-menu">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown">
+                <li className={`dropdown notifications-menu ${this.state.isNotificationDropDownOpen ? 'open' : ''}`}>
+                  <PointerStyledCursor onLinkClick={this.toggleNotificationDropDown} className="dropdown-toggle" data-toggle="dropdown" aria-expanded={this.state.isNotificationDropDownOpen}>
                     <i className="fa fa-bell-o"></i>
-                    <span className="label label-warning">10</span>
-                  </a>
+                    <span className="label label-warning">{this.props.notificationData.totalNotifications}</span>
+                  </PointerStyledCursor>
                   <ul className="dropdown-menu">
-                    <li className="header">You have 10 notifications</li>
+                    <li className="header">You have {this.props.notificationData.totalNotifications} notifications</li>
                     <li>
                       <ul className="menu">
-                        <li>
-                          <a href="#">
-                            <i className="fa fa-users text-aqua"></i> 5 new members joined today
-                          </a>
-                        </li>
+                        {
+                          this.props.notificationData.notifications.map((notification) => (
+                            <li key={`__notification_top_menu_${shortid.generate()}`}>
+                              <Link to={notification.link}>
+                                <i className="fa fa-users text-aqua"></i> {notification.text}
+                              </Link>
+                            </li>
+                          ))
+                        }
                       </ul>
                     </li>
-                    <li className="footer"><a href="#">View all</a></li>
+                    <li className="footer"><Link to="/" className="footer">View all</Link></li>
                   </ul>
                 </li>
-                <li className="dropdown tasks-menu">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-                    <i className="fa fa-flag-o"></i>
-                    <span className="label label-danger">9</span>
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li className="header">You have 9 tasks</li>
-                    <li>
-                      <ul className="menu">
-                        <li>
-                          <a href="#">
-                            <h3>
-                              Design some buttons
-                              <small className="pull-right">20%</small>
-                            </h3>
-                            <div className="progress xs">
-                              <div className="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar"
-                                  aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                                <span className="sr-only">20% Complete</span>
-                              </div>
-                            </div>
-                          </a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li className="footer">
-                      <a href="#">View all tasks</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="dropdown user user-menu">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-                    <img src="../../dist/img/user2-160x160.jpg" className="user-image" alt="User Image" />
-                    <span className="hidden-xs">Alexander Pierce</span>
-                  </a>
+                <li className={`dropdown user user-menu ${this.state.isProfileDropdownOpen ? 'open' : ''}`}>
+                  <PointerStyledCursor onLinkClick={this.toggleProfileDropdown} className="dropdown-toggle" data-toggle="dropdown" aria-expanded={this.state.isProfileDropdownOpen}>
+                    <img src={MaleAvatar} className="user-image" alt="User" />
+                    <span className="hidden-xs">{this.props.profileData.name}</span>
+                  </PointerStyledCursor>
                   <ul className="dropdown-menu">
                     <li className="user-header">
-                      <img src="../../dist/img/user2-160x160.jpg" className="img-circle" alt="User Image" />
-
+                      <img src={MaleAvatar} className="img-circle" alt="User" />
                       <p>
-                        Alexander Pierce - Web Developer
-                        <small>Member since Nov. 2012</small>
+                        {this.props.profileData.name} - {this.props.profileData.jobTitle}
+                        <small>Member since {this.props.profileData.memberSince}</small>
                       </p>
-                    </li>
-                    <li className="user-body">
-                      <div className="row">
-                        <div className="col-xs-4 text-center">
-                          <a href="#">Followers</a>
-                        </div>
-                        <div className="col-xs-4 text-center">
-                          <a href="#">Sales</a>
-                        </div>
-                        <div className="col-xs-4 text-center">
-                          <a href="#">Friends</a>
-                        </div>
-                      </div>
                     </li>
                     <li className="user-footer">
                       <div className="pull-left">
-                        <a href="#" className="btn btn-default btn-flat">Profile</a>
+                        <Link to="/" className="btn btn-default btn-flat">Profile</Link>
                       </div>
                       <div className="pull-right">
-                        <a href="#" className="btn btn-default btn-flat">Sign out</a>
+                        <Link to="/" className="btn btn-default btn-flat">Sign out</Link>
                       </div>
                     </li>
                   </ul>
-                </li>
-                <li>
-                  <a href="#" data-toggle="control-sidebar"><i className="fa fa-gears"></i></a>
                 </li>
               </ul>
             </div>
@@ -163,7 +176,9 @@ class TopMenu extends React.Component { // eslint-disable-line react/prefer-stat
 }
 
 TopMenu.propTypes = {
-
+  messageData: PropTypes.object.isRequired,
+  notificationData: PropTypes.object.isRequired,
+  profileData: PropTypes.object.isRequired,
 };
 
 export default TopMenu;
